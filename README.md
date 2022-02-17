b16a2add0f21c0886970efec91d9d1a613177d16
dymafr <erwan1309@hotmail.com>
2022-02-17 11:22:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-u81nbn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-u81nbn)
---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1 :class="h1Title" :id="h1id">Bonjour monde !</h1>
+</template>
+
+<script setup lang="ts">
+const h1Title = 'uneClasse';
+const h1id = 'unId';
+</script>
+
+<style></style>
